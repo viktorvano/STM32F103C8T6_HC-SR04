7754acea0c82a7500deb33359b2e70c34d038802
Viktor Vaňo <viktorvano@gmail.com>
2021-01-17 13:44:54 +0100

    Uploading project

---
+++ Core/Inc/myLibrary.h
@@ -0,0 +1,29 @@
+/*
+ * myLibrary.h
+ *
+ *  Created on: Dec 20, 2020
+ *      Author: vikto
+ */
+
+#ifndef INC_MYLIBRARY_H_
+#define INC_MYLIBRARY_H_
+
+#include "main.h"
+#include "usb_device.h"
+#include "usbd_cdc_if.h"
+
+extern TIM_HandleTypeDef htim2;
+
+#define TriggerDuration 2
+
+uint16_t distance, triggerTime, sensor;
+GPIO_TypeDef *triggerPorts[3];
+uint16_t triggerPins[3];
+GPIO_TypeDef *echoPorts[3];
+uint16_t echoPins[3];
+
+void SysTickEnable();
+void SysTickDisable();
+uint16_t measureDistance(GPIO_TypeDef *triggerPort, uint16_t triggerPin, GPIO_TypeDef *echoPort, uint16_t echoPin);
+
+#endif /* INC_MYLIBRARY_H_ */
---
+++ Core/Src/myLibrary.c
@@ -0,0 +1,46 @@
+/*
+ * myLibrary.c
+ *
+ *  Created on: Dec 20, 2020
+ *      Author: vikto
+ */
+
+
+#include "myLibrary.h"
+
+uint16_t distance = 0, triggerTime = 0, sensor = 0;
+GPIO_TypeDef *triggerPorts[3] = {Trigger0_GPIO_Port, Trigger1_GPIO_Port, Trigger2_GPIO_Port};
+uint16_t triggerPins[3] = {Trigger0_Pin, Trigger1_Pin, Trigger2_Pin};
+GPIO_TypeDef *echoPorts[3] = {Echo0_GPIO_Port, Echo1_GPIO_Port, Echo2_GPIO_Port};
+uint16_t echoPins[3] = {Echo0_Pin, Echo1_Pin, Echo2_Pin};
+
+void SysTickEnable()
+{
+	__disable_irq();
+	SysTick->CTRL |= (SysTick_CTRL_CLKSOURCE_Msk | SysTick_CTRL_ENABLE_Msk);
+	__enable_irq();
+}
+
+void SysTickDisable()
+{
+	__disable_irq();
+	SysTick->CTRL &= ~(SysTick_CTRL_CLKSOURCE_Msk | SysTick_CTRL_ENABLE_Msk);
+	__enable_irq();
+}
+
+uint16_t measureDistance(GPIO_TypeDef *triggerPort, uint16_t triggerPin, GPIO_TypeDef *echoPort, uint16_t echoPin)
+{
+	SysTickDisable();
+	HAL_TIM_Base_Start_IT(&htim2);
+	HAL_GPIO_WritePin(triggerPort, triggerPin, GPIO_PIN_SET);
+	triggerTime = 0;//reset the variable
+	asm ("nop");//to avoid program freezing
+	while(triggerTime < TriggerDuration);
+	HAL_GPIO_WritePin(triggerPort, triggerPin, GPIO_PIN_RESET);
+	while(!HAL_GPIO_ReadPin(echoPort, echoPin));
+	distance = 0;//reset the variable
+	while(HAL_GPIO_ReadPin(echoPort, echoPin));
+	HAL_TIM_Base_Stop_IT(&htim2);
+	SysTickEnable();
+	return distance;
+}
